b01559de0465d2688c125e354b08f16a274a9142
Brayical <124473807+Brayical@users.noreply.github.com>
2023-02-04 19:09:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+   <title></title>
+   <meta http-equiv="refresh" content="0; url = https://sites.google.com/view/oregonstate"/>
+</head>
+<body>
+   <h1></h1>
+   <p></p>
+</body>
+</html>
